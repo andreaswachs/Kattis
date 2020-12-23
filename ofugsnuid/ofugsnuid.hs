be4906f8a6ac84d861dd356bbc87a883d3ef5fe4
Andreas Wachs <andertraaks@gmail.com>
2020-12-23 20:28:59 +0100

    finished Reverse in haskell

--- ofugsnuid/ofugsnuid.hs
+++ ofugsnuid/ofugsnuid.hs
@@ -4,10 +4,22 @@
 -- brug debug sådan: ____KODE____ `debug` "tekst til konsol"
 -- brug :set +s i GHCI for execution times
 
+import System.IO ( isEOF)
+import Control.Monad (unless)
+    
 
+solve :: [String] -> IO ()
+solve xs = do
+    isDone <- isEOF
+    if not isDone then do
+        input <- getLine
+        solve (input : xs)
+    else do
+        putStr (unlines xs)
 
 main = do
     input <- getLine
+    solve []
     return 0
 
 
