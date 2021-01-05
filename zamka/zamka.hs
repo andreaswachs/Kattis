b7f8898ee15fcfebdde0ff79396d9bec79eac3a2
Andreas Wachs <andertraaks@gmail.com>
2021-01-05 23:58:08 +0100

    Finished zamka in Haskell

--- zamka/zamka.hs
+++ zamka/zamka.hs
@@ -1,12 +1,22 @@
--- import Debug.Trace -- udkommenter til debug. 
+-- import Debug.Trace -- udkommenter til debug.
 -- debug = flip trace -- hjælpefunktion til at printe med
+import Data.List (group)
+import Data.Char (digitToInt)
 
-solve xs = --TODO 
+solve :: [Int] -> [Int]
+solve (l:d:x:_) = [foundMin, foundMax]
+    where values = filter (\i -> digitSum i == x) [l..d]
+          foundMin = minimum values
+          foundMax = maximum values
 
-readInput :: String -> [Integer]
+digitSum :: Int -> Int
+digitSum = sum . map digitToInt . show
+
+
+readInput :: String -> [Int]
 readInput = (map read) . words
 
-writeOutput :: [Integer] -> String
+writeOutput :: [Int] -> String
 writeOutput = unlines . (map show)
 
 main = interact (writeOutput . solve . readInput)
